54d1a6f34aed21b67efb72966b5b06ae489c8984
NureKhodusDanylo <danylo.khodus@nure.ua>
2025-12-11 16:45:31 +0100

    edit ip

--- Task2/arkpz-pzpi-23-10-khodus-danylo-task2/robdelivery-web/src/lib/api.ts
+++ Task2/arkpz-pzpi-23-10-khodus-danylo-task2/robdelivery-web/src/lib/api.ts
@@ -13,7 +13,7 @@ import type {
   RobotEfficiency,
 } from '../types';
 
-const API_BASE_URL = 'http://http://35.192.164.131:5102/api';
+const API_BASE_URL = 'http://35.192.164.131:5102/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
